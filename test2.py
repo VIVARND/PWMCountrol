be6c3e2157f2a8e5707737706912e03a19c02a0f
VIVARND <vivarnd77@gmail.com>
2024-01-18 14:18:54 +0900

    Update test2.py
    pwm 테스트2 수정

--- test2.py
+++ test2.py
@@ -1,29 +1,26 @@
-import pigpio
+import RPi.GPIO as GPIO
 import time
 
 # RC 조종기 핀 설정
 rc_pin = 14  # RC 조종기 신호 핀 번호
 
 # GPIO 설정
-pi = pigpio.pi()
-
-if not pi.connected:
-    print("Could not connect to pigpio. Make sure pigpiod is running.")
-    exit()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(rc_pin, GPIO.IN)
 
 try:
     print("Reading PWM signals from RC controller...")
 
     while True:
-        pulse_width = pi.get_servo_pulsewidth(rc_pin, 10)  # 최근 10개의 샘플을 사용하여 중간값 계산
+        pulse_width = GPIO.input(rc_pin)
+        # GPIO.input() 함수는 0 또는 1을 반환하므로, 이 값을 사용하여 PWM 값을 읽을 수 있습니다.
 
-        if pulse_width > 0:
-            # 10번 채널 값 출력
-            print("Channel 10 PWM:", pulse_width)
+        # 10번 채널 값 출력
+        print("Channel 10 PWM:", pulse_width)
 
         time.sleep(0.1)
 
 except KeyboardInterrupt:
     # 프로그램 종료 시 GPIO 리소스 해제
-    pi.stop()
+    GPIO.cleanup()
     print("Program terminated.")
