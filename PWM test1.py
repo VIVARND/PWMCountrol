67438022fe39b89a6ca77d47db5a6f18caf9b636
VIVARND <vivarnd77@gmail.com>
2024-01-25 14:50:43 +0900

    Update PWM test1.py

--- PWM test1.py
+++ PWM test1.py
@@ -1,31 +1,30 @@
 import time
-import pigpio
+import RPi.GPIO as GPIO
+import keyboard
 
-ppm_pin = 18  # PPM 신호를 읽을 GPIO 핀 (라즈베리파이 3B/3B+/4B의 경우 GPIO18)
+pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀 (라즈베리파이 3B/3B+/4B의 경우 GPIO18)
 
-def ppm_callback(gpio, level, tick):
-    print("PPM 신호 감지:", level, "Tick:", tick)
+def pwm_callback(channel):
+    pulse_start = time.time()
+    while GPIO.input(channel) == GPIO.HIGH:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    print("채널 10 PWM 값:", pulse_duration)
 
-pi = pigpio.pi()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pwm_pin, GPIO.IN)
 
-if not pi.connected:
-    print("GPIO 초기화 실패. 프로그램을 종료합니다.")
-    exit()
-
-# PPM 신호를 읽기 위한 GPIO 핀 설정
-pi.set_mode(ppm_pin, pigpio.INPUT)
-pi.set_pull_up_down(ppm_pin, pigpio.PUD_UP)
-
-# PPM 신호 변화 감지 콜백 설정
-pi.callback(ppm_pin, pigpio.EITHER_EDGE, ppm_callback)
+GPIO.add_event_detect(pwm_pin, GPIO.BOTH, callback=pwm_callback)
 
 try:
     while True:
         time.sleep(0.5)
+        if keyboard.is_pressed('ctrl+c'):  # Ctrl+C를 누르면 프로그램 종료
+            break
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    pi.stop()
+    GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
