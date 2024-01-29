b9ec818f949e0b10b93f34ef4e57f902775a9712
VIVARND <vivarnd77@gmail.com>
2024-01-29 11:39:45 +0900

    Update servo_test1.py

--- servo_test1.py
+++ servo_test1.py
@@ -33,7 +33,8 @@ def pwm_callback(channel):
             set_servo_angle(0)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pwm_pin, GPIO.IN)  # PWM 핀을 입력으로 설정
+GPIO.setwarnings(False)  # 경고 메시지 비활성화
+GPIO.setup(pwm_pin, GPIO.OUT)  # PWM 핀을 출력으로 설정
 GPIO.setup(servo_pin, GPIO.OUT)  # 서보 모터 핀을 출력으로 설정
 
 pwm = GPIO.PWM(pwm_pin, 50)  # PWM 객체를 생성
