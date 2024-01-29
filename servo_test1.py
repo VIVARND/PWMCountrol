a12580a204002c1db0ba0806a0db126d10dde6d0
VIVARND <vivarnd77@gmail.com>
2024-01-29 15:49:09 +0900

    Update servo_test1.py

--- servo_test1.py
+++ servo_test1.py
@@ -1,13 +1,12 @@
 import time
 import RPi.GPIO as GPIO
-import sys
 
-pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀 (라즈베리파이 3B/3B+/4B의 경우 GPIO18)
-servo_pin = 24  # 서보 모터를 제어할 GPIO 핀 (라즈베리파이에 연결된 실제 핀 번호에 따라 수정하세요)
+pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀
+servo_pin = 24  # 서보 모터를 제어할 GPIO 핀
 
 def set_servo_angle(angle):
     if angle == 10:
-        pwm_value = 0.0090
+        pwm_value = 0.0009
     elif angle == 40:
         pwm_value = 0.0013
     elif angle == 90:
@@ -17,7 +16,6 @@ def set_servo_angle(angle):
     
     pwm.ChangeDutyCycle(pwm_value * 100)
     print("현재 서보모터 각도:", angle)
-    sys.stdout.flush()
 
 def pwm_callback(channel):
     pulse_start = time.time()
@@ -26,15 +24,15 @@ def pwm_callback(channel):
         pulse_end = time.time()
     pulse_duration = pulse_end - pulse_start
     if pulse_duration != 0.0:
-        if 0.0090 <= pulse_duration <= 0.0011:
+        if 0.0007 <= pulse_duration <= 0.0012:
             set_servo_angle(10)
-        elif 0.0012 <= pulse_duration <= 0.0014:
+        elif 0.0013 <= pulse_duration <= 0.0015:
             set_servo_angle(40)
-        elif 0.0015 <= pulse_duration <= 0.0019:
+        elif 0.0016 <= pulse_duration <= 0.0021:
             set_servo_angle(90)
         else:
             set_servo_angle(0)
-    print("PWM 값:", round(pulse_duration, 4))
+    print("PWM 값:", round(pulse_duration, 4))  # PWM 값 출력
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
