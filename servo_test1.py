e948e69a4774e867154656b74456a66e1b01d93e
VIVARND <vivarnd77@gmail.com>
2024-01-29 13:41:19 +0900

    Update servo_test1.py

--- servo_test1.py
+++ servo_test1.py
@@ -2,20 +2,25 @@ import time
 import RPi.GPIO as GPIO
 
 pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀 (라즈베리파이 3B/3B+/4B의 경우 GPIO18)
-servo_pin = 24  # 서보 모터를 제어할 GPIO 핀 (라즈베리파이에 연결된 실제 핀 번호에 따라 수정하세요)
+servo_pin = 24  # 서보모터 핀
 
-def set_servo_angle(angle):
-    if 0.0007 <= angle <= 0.0012:
-        angle = 10
-    elif 0.0013 <= angle <= 0.0015:
-        angle = 40
-    elif 0.0016 <= angle <= 0.0021:
-        angle = 90
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pwm_pin, GPIO.IN)
+GPIO.setup(servo_pin, GPIO.OUT)
+
+pwm = GPIO.PWM(pwm_pin, 50)  # PWM 객체를 생성, 주파수는 50Hz로 설정
+pwm.start(0)
+
+def set_servo_angle(pulse_duration):
+    if 0.0007 <= pulse_duration <= 0.0012:
+        angle = 10  # 10도
+    elif 0.0013 <= pulse_duration <= 0.0015:
+        angle = 40  # 40도
+    elif 0.0016 <= pulse_duration <= 0.0021:
+        angle = 90  # 90도
     else:
-        angle = 0
-    
-    pwm.ChangeDutyCycle(angle * 100)
-    print("현재 서보모터 각도:", angle)
+        angle = 90  # 기본값은 90도
+    print("서보모터 각도:", angle)
 
 def pwm_callback(channel):
     pulse_start = time.time()
@@ -26,21 +31,13 @@ def pwm_callback(channel):
     if pulse_duration != 0.0:
         set_servo_angle(pulse_duration)
     print("PWM 값:", round(pulse_duration, 3))  # 소수점 3자리까지 출력
+    time.sleep(0.5)  # 0.5초 간격으로 출력
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)  # 경고 메시지 비활성화
-GPIO.setup(pwm_pin, GPIO.OUT)  # PWM 핀을 출력으로 설정
-GPIO.setup(servo_pin, GPIO.OUT)  # 서보 모터 핀을 출력으로 설정
-
-pwm = GPIO.PWM(pwm_pin, 50)  # PWM 객체를 생성
-pwm.start(0)  # PWM 신호를 초기화
-
-GPIO.setup(pwm_pin, GPIO.IN)  # PWM 핀을 입력으로 설정
 GPIO.add_event_detect(pwm_pin, GPIO.BOTH, callback=pwm_callback)
 
 try:
     while True:
-        time.sleep(0.5)  # 0.5초마다 실행되도록 설정
+        pass
 except KeyboardInterrupt:
     pass
 finally:
