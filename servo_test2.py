c22b720c9f4942c525c1d6bc2a1586ebb31496ac
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-01-31 16:21:19 +0900

    Update servo_test2.py
    servo test2

--- servo_test2.py
+++ servo_test2.py
@@ -2,6 +2,12 @@ import time
 import RPi.GPIO as GPIO
 
 pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀
+servo_pin = 24  # 서보 모터를 제어할 GPIO 핀
+
+def set_servo_angle(angle):
+    pwm_value = angle / 180.0 * (2000 - 900) + 900
+    pwm.ChangeDutyCycle(pwm_value / 20.0)  # PWM 신호 변경
+    print(f"현재 서보모터 각도: {angle}도, PWM 값: {round(pwm_value)}")
 
 def pwm_callback(channel):
     pulse_start = time.time()
@@ -11,11 +17,23 @@ def pwm_callback(channel):
     pulse_duration = pulse_end - pulse_start
     if pulse_duration != 0.0:
         pwm_value = round(pulse_duration * 1000000)  # PWM 값 변환 (마이크로초로 변환)
-        print("PWM 값:", pwm_value)  # PWM 값 출력
+        if 900 <= pwm_value <= 1200:
+            set_servo_angle(10)
+        elif 1300 <= pwm_value <= 1500:
+            set_servo_angle(50)
+        elif 1800 <= pwm_value <= 2000:
+            set_servo_angle(90)
+        else:
+            print("다른 신호가 들어왔습니다. 서보 모터 동작을 잠시 멈춥니다.")
+            time.sleep(1)  # 1초 동안 동작을 멈춥니다.
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(pwm_pin, GPIO.IN)  # PWM 핀을 입력으로 설정
+GPIO.setup(servo_pin, GPIO.OUT)
+
+pwm = GPIO.PWM(servo_pin, 50)  # 서보 모터 PWM 설정
+pwm.start(0)
 
 try:
     GPIO.add_event_detect(pwm_pin, GPIO.BOTH, callback=pwm_callback)
@@ -24,5 +42,6 @@ try:
 except KeyboardInterrupt:
     pass
 finally:
+    pwm.stop()
     GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
