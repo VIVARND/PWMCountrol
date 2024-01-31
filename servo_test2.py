3b5c5e92968a3d580cd652bf259ee0785aae530d
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-01-31 16:43:44 +0900

    Update servo_test2.py

--- servo_test2.py
+++ servo_test2.py
@@ -7,13 +7,7 @@ servo_pin = 24  # 서보 모터를 제어할 GPIO 핀
 def set_servo_angle(angle):
     pwm_value = angle / 180.0 * (2000 - 900) + 900
     pwm.ChangeDutyCycle(pwm_value / 20.0)  # PWM 신호 변경
-    print(f"서보모터 각도: {angle}도, PWM 값: {round(pwm_value)}")
-
-def move_servo(start_angle, end_angle, step, delay):
-    direction = 1 if end_angle > start_angle else -1
-    for angle in range(start_angle, end_angle, direction * step):
-        set_servo_angle(angle)
-        time.sleep(delay)
+    print(f"현재 서보모터 각도: {angle}도, PWM 값: {round(pwm_value)}")
 
 def pwm_callback(channel):
     pulse_start = time.time()
@@ -23,11 +17,15 @@ def pwm_callback(channel):
     pulse_duration = pulse_end - pulse_start
     if pulse_duration != 0.0:
         pwm_value = round(pulse_duration * 1000000)  # PWM 값 변환 (마이크로초로 변환)
-        print("현재 PWM 값:", pwm_value)  # PWM 값 출력
-        if 1800 <= pwm_value <= 2000:
-            move_servo(180, 10, 10, 0.1)  # 180도에서 10도로 이동
+        if 900 <= pwm_value <= 1200:
+            set_servo_angle(10)
+        elif 1300 <= pwm_value <= 1500:
+            set_servo_angle(50)
+        elif 1800 <= pwm_value <= 2000:
+            set_servo_angle(90)
         else:
-            move_servo(10, 180, 10, 0.1)  # 10도에서 180도로 이동
+            print("다른 신호가 들어왔습니다. 서보 모터 동작을 잠시 멈춥니다.")
+            time.sleep(1)  # 1초 동안 동작을 멈춥니다.
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -47,3 +45,4 @@ finally:
     pwm.stop()
     GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
+    
