ef4ad1464d2a7f1b6ae560b849e11ca6bf20f68f
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-02-05 10:30:35 +0900

    Update dc_test2.py

--- dc_test2.py
+++ dc_test2.py
@@ -1,35 +1,47 @@
 import RPi.GPIO as GPIO
 import time
 
-motor_in1_pin = 17  # DIR 핀
-motor_pwm_pin = 18  # PWM 핀
-SPEED = 100  # 원하는 속도 (0 ~ 100)
+pwm_pin = 17  # PWM 신호를 읽을 GPIO 핀
+motor_in1_pin = 18  # DIR 핀
+SPEED_MIN = 900
+SPEED_MAX = 1100
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+GPIO.setup(pwm_pin, GPIO.IN)
 GPIO.setup(motor_in1_pin, GPIO.OUT)
-GPIO.setup(motor_pwm_pin, GPIO.OUT)
+GPIO.output(motor_in1_pin, GPIO.LOW)  # 초기에는 모터 OFF로 설정
 
-pwm = GPIO.PWM(motor_pwm_pin, 100)  # PWM 주파수를 100Hz로 설정
-pwm.start(0)
+def control_dc_motor(pwm_value, speed):
+    if SPEED_MIN <= pwm_value <= SPEED_MAX:
+        # PWM 값에 따라 모터 상태 결정
+        GPIO.output(motor_in1_pin, GPIO.HIGH)  # 모터 ON
+        print(f"DC 모터 ON - 속도: {speed}%")
+    else:
+        GPIO.output(motor_in1_pin, GPIO.LOW)  # 모터 OFF
+        print("DC 모터 OFF")
 
 try:
     while True:
-        # 전진 및 일정한 속도 조절
-        GPIO.output(motor_in1_pin, GPIO.HIGH)
-        pwm.ChangeDutyCycle(SPEED)
-        time.sleep(2)
+        pulse_start = time.time()
+        pulse_end = pulse_start
+        while GPIO.input(pwm_pin) == GPIO.HIGH:
+            pulse_end = time.time()
+        pulse_duration = pulse_end - pulse_start
 
-        # 정지
-        GPIO.output(motor_in1_pin, GPIO.LOW)
-        pwm.ChangeDutyCycle(0)
-        time.sleep(1)
+        if pulse_duration != 0.0:
+            pwm_value = round(pulse_duration * 1000000)  # PWM 값 변환 (마이크로초로 변환)
+            speed = (pwm_value - SPEED_MIN) / (SPEED_MAX - SPEED_MIN) * 100  # 속도 계산
+            print("PWM 값:", pwm_value)
+
+            # PWM 값에 따라 DC 모터 상태 결정
+            control_dc_motor(pwm_value, speed)
+        time.sleep(0.5)  # 0.5초 간격으로 PWM 값을 확인
 
 except KeyboardInterrupt:
     pass
 
 finally:
-    pwm.stop()
     GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
