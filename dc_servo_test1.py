2bca17aad3f3a81dfb5d8723bc00fcd86504c569
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-02-05 16:54:40 +0900

    Update dc_servo_test1.py

--- dc_servo_test1.py
+++ dc_servo_test1.py
@@ -2,8 +2,8 @@ import time
 import RPi.GPIO as GPIO
 
 pwm_pin_from_receiver = 17  # R3008SB의 PWM 신호를 읽을 GPIO 핀
-motor_pwm_pin = 18  # DC 모터 PWM 핀
-motor_in1_pin = 22  # DC 모터 제어 핀
+dc_motor_pwm_pin = 18  # DC 모터 PWM 핀
+dc_motor_in1_pin = 22  # DC 모터 제어 핀
 
 servo_pwm_pin = 23  # PWM 신호를 읽을 GPIO 핀
 servo_pin = 24  # 서보 모터를 제어할 GPIO 핀
@@ -16,35 +16,35 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(pwm_pin_from_receiver, GPIO.IN)
-GPIO.setup(motor_pwm_pin, GPIO.OUT)
-GPIO.setup(motor_in1_pin, GPIO.OUT)
-GPIO.output(motor_in1_pin, GPIO.LOW)  # 모터를 A방향으로 설정
+GPIO.setup(dc_motor_pwm_pin, GPIO.OUT)
+GPIO.setup(dc_motor_in1_pin, GPIO.OUT)
+GPIO.output(dc_motor_in1_pin, GPIO.LOW)  # DC 모터를 A방향으로 설정
 
 GPIO.setup(servo_pwm_pin, GPIO.IN)  # PWM 핀을 입력으로 설정
 GPIO.setup(servo_pin, GPIO.OUT)
 
-motor_pwm = GPIO.PWM(motor_pwm_pin, 100)  # DC 모터 PWM 설정
-motor_pwm.start(0)
+dc_motor_pwm = GPIO.PWM(dc_motor_pwm_pin, 100)  # DC 모터 PWM 설정
+dc_motor_pwm.start(0)
 
 servo_pwm = GPIO.PWM(servo_pin, 50)  # 서보 모터 PWM 설정
 servo_pwm.start(0)
 
 def control_dc_motor(speed):
     if speed == 0:
-        motor_pwm.ChangeDutyCycle(0)  # 모터 OFF
-        print("DC 모터 OFF")
+        dc_motor_pwm.ChangeDutyCycle(0)  # 모터 OFF
+        print("PWM1 - DC 모터 OFF")
     else:
-        motor_pwm.ChangeDutyCycle(speed)  # 속도값 사용
-        print(f"DC 모터 ON - 속도: {speed:.1f}%")
+        dc_motor_pwm.ChangeDutyCycle(speed)  # 속도값 사용
+        print(f"PWM1 - DC 모터 ON - 속도: {speed:.1f}%")
 
 def set_servo_angle(angle):
     duty_cycle = angle / 18.0 + 2.5  # 각도에 따른 PWM 듀티 사이클 계산
     servo_pwm.ChangeDutyCycle(duty_cycle)
-    print(f"현재 서보모터 각도: {angle}도")
+    print(f"PWM2 - 현재 서보모터 각도: {angle}도")
 
 def stop_servo():
     servo_pwm.ChangeDutyCycle(0)  # PWM 신호를 0으로 설정 (서보 모터 정지)
-    print("서보모터 정지")
+    print("PWM2 - 서보모터 정지")
 
 def pwm_callback(channel):
     pulse_start = time.time()
@@ -85,7 +85,7 @@ try:
 except KeyboardInterrupt:
     pass
 finally:
-    motor_pwm.stop()
+    dc_motor_pwm.stop()
     servo_pwm.stop()
     GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
