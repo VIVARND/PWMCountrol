4017fdf22f8a9fd6e5cd20abd6e921934b4dd52d
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-02-05 15:08:09 +0900

    Update dc_test2.py

--- dc_test2.py
+++ dc_test2.py
@@ -2,22 +2,32 @@ import RPi.GPIO as GPIO
 import time
 
 pwm_pin_from_receiver = 17  # R3008SB의 PWM 신호를 읽을 GPIO 핀
-motor_in1_pin = 18  # DIR 핀
+motor_pwm_pin = 18  # DC 모터 PWM 핀
+motor_in1_pin = 22  # DC 모터 제어 핀
 
 SPEED_MIN = 1200
 SPEED_MAX = 1900
-SPEED_STEP = 0.1
+SPEED_STEP = 10
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(pwm_pin_from_receiver, GPIO.IN)
+GPIO.setup(motor_pwm_pin, GPIO.OUT)
 GPIO.setup(motor_in1_pin, GPIO.OUT)
 GPIO.output(motor_in1_pin, GPIO.LOW)  # 초기에는 모터 OFF로 설정
 
+pwm = GPIO.PWM(motor_pwm_pin, 100)  # PWM 주파수를 100Hz로 설정
+pwm.start(0)
+
 def control_dc_motor(speed):
-    GPIO.output(motor_in1_pin, GPIO.HIGH)  # 모터 ON
-    print(f"DC 모터 ON - 속도: {speed:.1f}%")
+    if speed == 0:
+        GPIO.output(motor_in1_pin, GPIO.LOW)  # 모터 OFF
+        print("DC 모터 OFF")
+    else:
+        GPIO.output(motor_in1_pin, GPIO.HIGH)  # 모터 ON
+        pwm.ChangeDutyCycle(speed)
+        print(f"DC 모터 ON - 속도: {speed}%")
 
 try:
     while True:
@@ -35,15 +45,12 @@ try:
             print("PWM 값:", pwm_value)
 
             # PWM 값에 따라 DC 모터 상태 결정
-            if pwm_value < SPEED_MIN:
-                GPIO.output(motor_in1_pin, GPIO.LOW)  # 모터 OFF
-                print("DC 모터 OFF")
-            else:
-                control_dc_motor(speed)
+            control_dc_motor(speed)
 
 except KeyboardInterrupt:
     pass
 
 finally:
+    pwm.stop()
     GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
