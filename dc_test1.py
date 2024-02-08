557a73e816362f60e11417e25e4a6023d71e8784
VIVARND <149641928+VIVARND@users.noreply.github.com>
2024-02-08 12:34:40 +0900

    Update dc_test1.py

--- dc_test1.py
+++ dc_test1.py
@@ -1,15 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 
+# GPIO 설정 경고 비활성화
+GPIO.setwarnings(False)
+
 # GPIO 핀 설정
-MOTOR_PIN = 27  # 내장형 DC 모터를 제어하기 위한 GPIO 핀
-RC_PIN = 22   # 수신기의 PWM 신호를 받기 위한 GPIO 핀
+RC_PIN = 22  # 수신기의 신호선에 연결
+DC_MOTOR_PIN = 27  # DC 모터의 신호선에 연결
+
+# PWM 값을 읽어와 DC 모터를 제어하는 클래스
+class MotorControl:
+    def __init__(self, dc_motor_pin):
+        self.dc_motor_pin = dc_motor_pin
+        GPIO.setup(self.dc_motor_pin, GPIO.OUT)
+        GPIO.output(self.dc_motor_pin, GPIO.LOW)
+
+    def set_motor_state(self, pwm_value):
+        # 범위에 따라 DC 모터를 ON 또는 OFF로 설정
+        if 950 <= pwm_value <= 1100:
+            GPIO.output(self.dc_motor_pin, GPIO.HIGH)  # DC 모터를 ON으로 설정
+        else:
+            GPIO.output(self.dc_motor_pin, GPIO.LOW)  # DC 모터를 OFF로 설정
 
 # GPIO 설정
-GPIO.setwarnings(False)  # 경고 끄기
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(MOTOR_PIN, GPIO.OUT)
-GPIO.setup(RC_PIN, GPIO.IN)  # 수신기의 PWM 신호를 입력으로 설정
+GPIO.setup(RC_PIN, GPIO.IN)  # 입력 모드로 설정
+
+# DC 모터 제어 객체 생성
+dc_motor_control = MotorControl(DC_MOTOR_PIN)
 
 try:
     while True:
@@ -25,16 +43,11 @@ try:
 
         pwm_value = round(pulse_duration * 1000000)
         
-        # 현재 PWM 값과 상태 출력
+        # PWM 값 출력
         print(f"현재 PWM 값: {pwm_value:04d}")
         
-        # 범위에 따라 DC 모터 상태 제어
-        if 900 <= pwm_value <= 1200:
-            print("DC 모터 ON")
-            GPIO.output(MOTOR_PIN, GPIO.HIGH)  # 모터 ON
-        else:
-            print("DC 모터 OFF")
-            GPIO.output(MOTOR_PIN, GPIO.LOW)   # 모터 OFF
+        # DC 모터 제어
+        dc_motor_control.set_motor_state(pwm_value)
 
         time.sleep(0.1)  # 갱신 주기에 따라 조절
 
