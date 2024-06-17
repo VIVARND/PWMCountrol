529256a26a258da025256f1ed446506755c91528
VIVARND <vivarnd77@gmail.com>
2024-06-17 11:25:19 +0900

    Update pwm_test1.py

--- pwm_test1.py
+++ pwm_test1.py
@@ -1,27 +1,28 @@
 import time
 import RPi.GPIO as GPIO
 
-pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀 (라즈베리파이 3B/3B+/4B의 경우 GPIO18)
+pwm_pin = 18  # PWM 신호를 읽을 GPIO 핀
 
 def pwm_callback(channel):
     pulse_start = time.time()
+    pulse_end = pulse_start
     while GPIO.input(channel) == GPIO.HIGH:
         pulse_end = time.time()
     pulse_duration = pulse_end - pulse_start
-    print("채널 10 PWM 값:", pulse_duration)
+    if pulse_duration != 0.0:
+        pwm_value = round(pulse_duration * 1000000)  # PWM 값 변환 (마이크로초로 변환)
+        print("PWM 값:", pwm_value)  # PWM 값 출력
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pwm_pin, GPIO.IN)
-
-GPIO.add_event_detect(pwm_pin, GPIO.BOTH, callback=pwm_callback)
+GPIO.setwarnings(False)
+GPIO.setup(pwm_pin, GPIO.IN)  # PWM 핀을 입력으로 설정
 
 try:
+    GPIO.add_event_detect(pwm_pin, GPIO.BOTH, callback=pwm_callback)
     while True:
-        time.sleep(0.1)  # 주기적으로 PWM 값을 확인하는 주기를 늦추었습니다.
-
+        time.sleep(0.5)
 except KeyboardInterrupt:
     pass
-
 finally:
     GPIO.cleanup()
     print("GPIO 정리 완료. 프로그램 종료.")
